a077b60b52393de4ec7289313ca58397f47f07a8
tharun <kkt01vel@gmail.com>
2023-08-15 22:00:28 +0530

    solution

---
+++ demo.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+print("hello world")
+n=int (input("enter a number "))
+m=int (input("enter another number "))
+print("addition of two numbers" ,add(n,m))
+print("subtraction of two numbers ",subtract(n,m))
